71642f038134ebd5aa025ad6a4b6cf4f78533178
Rafa <rafacv23@gmail.com>
2025-06-13 14:24:45 +0200

    Enhance header component with navigation links and search input; add game page with dynamic metadata generation; update layout metadata for improved user experience; clean up home page structure; update package dependencies.

--- frontend/react/app/components/header.tsx
+++ frontend/react/app/components/header.tsx
@@ -1,12 +1,28 @@
+import Link from "next/link"
+import { ShoppingCart, User } from "lucide-react"
+
 export default function Header() {
   return (
-    <header className="bg-header py-6 items-center justify-center mx-auto flex gap-4">
+    <header className="bg-header py-6 items-center justify-center mx-auto flex gap-4 sticky top-0 z-10">
       <img
-        src="header_logo.png"
+        src="/header_logo.png"
         alt="Steam logo for the header of the page"
         className="h-8"
       />
-      <p>A simple clone of the Steam website</p>
+      <Link href="/">Store</Link>
+      <Link href="/library">Library</Link>
+      <Link href="/community">Community</Link>
+      <Link href="/news">News</Link>
+      <input type="search" name="" id="" placeholder="Search for games" />
+      <Link href="/cart">
+        <ShoppingCart />
+      </Link>
+      <Link href="/profile">
+        <User />
+      </Link>
+      <Link href="/install" className="bg-primary px-4 py-2 rounded-md">
+        Install Steam
+      </Link>
     </header>
   )
 }
